1e27050f543aec41a530b4c66702e7fd8a422c24
Gleb Bezborodov <bezborodoff.gleb@gmail.com>
2022-04-17 13:22:49 +0300

    a few changes in drawing *skeleton

--- sample/potentials/main.cpp
+++ sample/potentials/main.cpp
@@ -11,24 +11,32 @@
 #include "potential_computing.h"
 
 void InitColors(rocky::IRockyConsole* console);
-void Init(rocky::IRockyConsole* console);
-void Draw(rocky::IRockyConsole* console);
+void Draw(rocky::IRockyConsole* console, const potential_computing::t_matrix& matrix);
 void CharAt(int ch, int color, int x, int y, rocky::IRockyConsole* console);
-void GetKey(rocky::IRockyConsole* console)
+void Move(rocky::RockyKey key, rocky::IRockyConsole* console);
+void SetPotential(rocky::RockyKey key, potential_computing& computer, rocky::IRockyConsole* console);
 
-int main(int argc, char* argv[])
-{
+int main(int argc, char* argv[]) {
     auto* console = rocky::Create();
 
     console->Initialize();
     //console->HideCursor();
-    bool isPressed = false;
-    Init(console);
-    while(true) {
-        if(isPressed)
-            GetKey(console);
+    InitColors(console);
+
+    unsigned max_x, max_y;
+    console->ObtainMaxXY(max_x, max_y);
+    potential_computing computer(max_x, max_y);
+    
+    Draw(console, computer.get_matrix());
+
+    while(true) {   
+        if(console->IsKeyPressed()){
+            auto key = console->GetKey();
+            if(key == rocky::RockyKey::Escape) break;
+            Move(key, console);
+
+        }
     }
-    //m_console->ObtainMaxXY(max_x, max_y);
 
     console->Destroy();
 
@@ -36,26 +44,35 @@ int main(int argc, char* argv[])
 }
 
 void CharAt(int ch, int color, int x, int y, rocky::IRockyConsole* console) {
+    unsigned cur_x, cur_y;
+    console->ObtainXY(cur_x, cur_y);
     console->GoToXY(x, y);
     console->SetColor(color);
     console->SetText("%c", ch);
+    console->GoToXY(cur_x, cur_y);
 }
 
-void Init(rocky::IRockyConsole* console) {
-    InitColors(console);
-    Draw(console);
-}
-
-void Draw(rocky::IRockyConsole* console, potential_computing::t_matrix& matrix) {
-    for(auto i_y = 0; i_y < matrix.size(); i_y++)
-        for(auto i_x = 0; i_x < matrix[i_y].size(); i_x++)
-            CharAt(' ', 3, i_x, i_y, console);
+void Draw(rocky::IRockyConsole* console, const potential_computing::t_matrix& matrix) {
+    for(auto i_y = 0; i_y < matrix.size(); i_y++) {
+        for(auto i_x = 0; i_x < matrix[i_y].size(); i_x++) {
+            auto color = matrix[i_y][i_x]; 
+            CharAt(' ', (int)color + 2, i_x, i_y, console);
+        }
+    } 
 }
 
 void InitColors(rocky::IRockyConsole* console) {
-    console->InitColor(5, rocky::RockyColor::Red, rocky::RockyColor::Red);
-    console->InitColor(4, rocky::RockyColor::Yellow, rocky::RockyColor::Yellow);
-    console->InitColor(3, rocky::RockyColor::White, rocky::RockyColor::White);
-    console->InitColor(2, rocky::RockyColor::Cyan, rocky::RockyColor::Cyan);
-    console->InitColor(1, rocky::RockyColor::Blue, rocky::RockyColor::Blue);
+    console->InitColor(4, rocky::RockyColor::Red, rocky::RockyColor::Red);
+    console->InitColor(3, rocky::RockyColor::Yellow, rocky::RockyColor::Yellow);
+    console->InitColor(2, rocky::RockyColor::White, rocky::RockyColor::White);
+    console->InitColor(1, rocky::RockyColor::Cyan, rocky::RockyColor::Cyan);
+    console->InitColor(0, rocky::RockyColor::Blue, rocky::RockyColor::Blue);
+}
+
+void Move(rocky::RockyKey key, rocky::IRockyConsole* console){
+    // TODO move cursor
+}
+
+void SetPotential(rocky::RockyKey key, potential_computing& computer, rocky::IRockyConsole* console){
+    // TODO set potential
 }
