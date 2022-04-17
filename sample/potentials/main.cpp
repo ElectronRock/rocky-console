b75ac73cf011a8bd22cefcfd8f02e2b9279c4ebc
Gleb Bezborodov <bezborodoff.gleb@gmail.com>
2022-04-17 14:56:27 +0300

    visualization

--- sample/potentials/main.cpp
+++ sample/potentials/main.cpp
@@ -14,13 +14,19 @@ void InitColors(rocky::IRockyConsole* console);
 void Draw(rocky::IRockyConsole* console, const potential_computing::t_matrix& matrix);
 void CharAt(int ch, int color, int x, int y, rocky::IRockyConsole* console);
 void Move(rocky::RockyKey key, rocky::IRockyConsole* console);
-void SetPotential(rocky::RockyKey key, potential_computing& computer, rocky::IRockyConsole* console);
+bool SetPotential(rocky::RockyKey key, potential_computing& computer, rocky::IRockyConsole* console);
+
+template<typename T>
+T clamp(const T v, const T l, const T r){
+    if(v < l) return l;
+    if(v > r) return r;
+    return v;
+}
 
 int main(int argc, char* argv[]) {
     auto* console = rocky::Create();
 
     console->Initialize();
-    //console->HideCursor();
     InitColors(console);
 
     unsigned max_x, max_y;
@@ -34,7 +40,10 @@ int main(int argc, char* argv[]) {
             auto key = console->GetKey();
             if(key == rocky::RockyKey::Escape) break;
             Move(key, console);
-
+            if(SetPotential(key, computer, console)){
+                computer.compute();
+                Draw(console, computer.get_matrix());
+            }      
         }
     }
 
@@ -56,23 +65,48 @@ void Draw(rocky::IRockyConsole* console, const potential_computing::t_matrix& ma
     for(auto i_y = 0; i_y < matrix.size(); i_y++) {
         for(auto i_x = 0; i_x < matrix[i_y].size(); i_x++) {
             auto color = matrix[i_y][i_x]; 
-            CharAt(' ', (int)color + 2, i_x, i_y, console);
+            CharAt(' ', clamp((int)color + 2, 0, 4), i_x, i_y, console);
         }
-    } 
+    }
+    console->HideCursor();
 }
 
 void InitColors(rocky::IRockyConsole* console) {
     console->InitColor(4, rocky::RockyColor::Red, rocky::RockyColor::Red);
-    console->InitColor(3, rocky::RockyColor::Yellow, rocky::RockyColor::Yellow);
+    console->InitColor(3, rocky::RockyColor::Green, rocky::RockyColor::Yellow);
     console->InitColor(2, rocky::RockyColor::White, rocky::RockyColor::White);
     console->InitColor(1, rocky::RockyColor::Cyan, rocky::RockyColor::Cyan);
     console->InitColor(0, rocky::RockyColor::Blue, rocky::RockyColor::Blue);
 }
 
-void Move(rocky::RockyKey key, rocky::IRockyConsole* console){
-    // TODO move cursor
+void Move(rocky::RockyKey key, rocky::IRockyConsole* console) {
+    int shift_x = 0, shift_y = 0;
+    if(key== rocky::RockyKey::Left) 
+        shift_x = -1;
+    if(key== rocky::RockyKey::Right) 
+        shift_x = 1;
+    if(key== rocky::RockyKey::Up) 
+        shift_y = -1;
+    if(key== rocky::RockyKey::Down) 
+        shift_y = 1;
+
+    if(shift_x != 0 || shift_y != 0){
+        unsigned x, y;
+        console->ObtainXY(x, y);
+        x += shift_x;
+        y += shift_y;
+        console->GoToXY(x, y);
+        console->ShowCursor();
+    }
 }
 
-void SetPotential(rocky::RockyKey key, potential_computing& computer, rocky::IRockyConsole* console){
-    // TODO set potential
+bool SetPotential(rocky::RockyKey key, potential_computing& computer, rocky::IRockyConsole* console) {
+    if(key != rocky::RockyKey::P && key != rocky::RockyKey::M)
+        return false;
+
+    unsigned x, y;
+    console->ObtainXY(x, y);
+    charge ch{ key == rocky::RockyKey::M ? -1.0 : 1.0, (double)x, (double)y };
+    computer.init_charge(ch);
+    return true;
 }
